544e6fa67fd105fc9fd987e89c3d37dd92392783
Pkjeevan <pkjeevanbabu@gmail.com>
2026-09-18 13:23:51 +0530

    Update mount_uuid.sh

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -2,27 +2,24 @@
 
 # ==========================================
 # Mount Filesystem Using UUID
-# Student Name:
-# Roll Number:
+# Student Name: jeevanbabu
+# Roll Number:1U24IT046
 # ==========================================
 
-
 # Display filesystem UUID
-
-
+blkid
 
 
 # Create mount directory
-
-
+mkdir -p /mnt/mydisk
 
 
 # Mount filesystem using UUID
 # Replace YOUR_UUID with actual UUID
-
-
+mount UUID=YOUR_UUID /mnt/mydisk
 
 
 # Display mounted filesystem
+df -h /mnt/mydisk
 
-
+exit 0
